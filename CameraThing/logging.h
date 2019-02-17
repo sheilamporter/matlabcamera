ef0c504f6dc6a89c396958dcbf2f09d31065d8b2
sheila <sirocelott@gmail.com>
2019-02-17 15:39:53 -0500

    switching logging to matlab console because files are a bust

--- CameraThing/logging.h
+++ CameraThing/logging.h
@@ -1,37 +1,25 @@
 #ifndef LOGGING_INCLUDED
 #define LOGGING_INCLUDED
 
-#include <fstream>
-#include <iostream>
+#include <sstream>
 
 #include "utils.h"
+
+#include "mex.h"
 
 using namespace std;
 
 // change this to false if you don't want this to output log files.
 // will probably speed up performance.
-static bool LOG = true;
-static ofstream logfile;
-
-void setupLog()
-{
-   if (!LOG)
-      return;
-   logfile.open(outputDir() + "cameralog.txt");
-}
-
-void teardownLog()
-{
-   if (!LOG)
-      return;
-   logfile.close();
-}
+bool LOG = true;
 
 void log(string output)
 {
    if (!LOG)
       return;
-   logfile << "[" << currentTime().c_str() << "] " << output.c_str() << "\n";
+   stringstream log;
+   log << "[" << currentTime().c_str() << "] " << output.c_str() << "\n";
+   mexPrintf(log.str().c_str());
 }
 
 
