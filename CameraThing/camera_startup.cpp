eb84f5a1bee516956cf0db4d7ce841ca1b84f73f
sheila <sirocelott@gmail.com>
2019-02-17 16:47:35 -0500

    lol let's try to do some camera shit rip me

--- CameraThing/camera_startup.cpp
+++ CameraThing/camera_startup.cpp
@@ -1,6 +1,7 @@
 
 #include <sstream>
 
+#include "uEye.h"
 #include "mex.h"
 
 #include "logging.h"
@@ -15,7 +16,6 @@ void startupCamera(const int cameraHandle)
    logtext << "Starting up camera with ID " << cameraHandle << "...";
    log(logtext.str());
 
-   /*
    //---------------------------------------------------------------------------
    // Open camera with provided ID
    // STOLEN FROM file:///C:/Program%20Files/IDS/uEye/Help/uEye_Manual/index.html?is_initcamera.html
@@ -24,16 +24,16 @@ void startupCamera(const int cameraHandle)
    INT nRet = is_InitCamera(&hCam, NULL);
    if (nRet != IS_SUCCESS)
    {
-   //Check if GigE uEye SE needs a new starter firmware
-   if (nRet == IS_STARTER_FW_UPLOAD_NEEDED)
-   {
-   log("ERROR: Camera failed to start up because it requires a firmware update.");
-   }
-   else
-   {
-   log("ERROR: Camera failed to start up with return code " + nRet);
-   }
-   return;
+      //Check if GigE uEye SE needs a new starter firmware
+      if (nRet == IS_STARTER_FW_UPLOAD_NEEDED)
+      {
+         log("ERROR: Camera failed to start up because it requires a firmware update.");
+      }
+      else
+      {
+         log("ERROR: Camera failed to start up with return code " + nRet);
+      }
+      return;
    }
    //---------------------------------------------------------------------------
 
@@ -41,18 +41,17 @@ void startupCamera(const int cameraHandle)
    nRet = is_SetDisplayMode(&hCam, IS_SET_DM_DIRECT3D);
    if (nRet != IS_SUCCESS)
    {
-   log("ERROR: Failed to set display mode with return code " + nRet);
-   return;
+      error("Failed to set display mode with return code " + nRet);
+      return;
    }
 
    // set trigger mode
    nRet = is_SetExternalTrigger(&hCam, IS_SET_TRIGGER_SOFTWARE);
    if (nRet != IS_SUCCESS)
    {
-   log("ERROR: Failed to set display mode with return code " + nRet);
-   return;
+      error("Failed to set display mode with return code " + nRet);
+      return;
    }
-   */
 
    log("...camera started up successfully.");
 }
