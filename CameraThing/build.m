2a89f979e08c4e7fe0b0d08c94b2cfa7f20c60f5
sheila <sirocelott@gmail.com>
2019-02-20 19:35:47 -0500

    updating build.m to point to ueye lib

--- CameraThing/build.m
+++ CameraThing/build.m
@@ -1,6 +1,6 @@
 disp("building camera_startup.cpp")
-mex camera_startup.cpp
+mex camera_startup.cpp uEye_api_64.lib
 disp("building camera_capture.cpp")
-mex camera_capture.cpp
+mex camera_capture.cpp uEye_api_64.lib
 disp("building camera_teardown.cpp")
-mex camera_teardown.cpp
+mex camera_teardown.cpp uEye_api_64.lib
